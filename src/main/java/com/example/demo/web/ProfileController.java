bf41e65affbabe770023173b8498ca7f964457f0
fumika.kanemitsu <fumika.kanemitsu@rakus.co.jp>
2017-06-14 15:35:12 +0900

    コントローラクラスを作成

--- src/main/java/com/example/demo/web/ProfileController.java
+++ src/main/java/com/example/demo/web/ProfileController.java
@@ -1,5 +1,33 @@
 package com.example.demo.web;
 
-public class ProfileController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+@RequestMapping("/")
+public class ProfileController {
+	@RequestMapping("/index")
+	public String index(@RequestParam Integer param, Model model) {
+		if (param == 1) {
+			return "adati";
+		}
+		if (param == 2) {
+			return "adati";
+		}
+		if (param == 3) {
+			return "adati";
+		}
+		if (param == 4) {
+			return "adati";
+		}
+		if (param == 5) {
+			return "adati";
+		}
+		if (param == 6) {
+			return "adati";
+		}
+		return null;
+	}
 }
